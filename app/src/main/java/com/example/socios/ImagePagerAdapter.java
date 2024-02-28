9a0671b1680a3ef45a415d6354f6a2aff19b5e08
Angel Geovanni Marcial Morales <Skatepanuco@gmail.com>
2024-02-27 21:54:14 -0600

    carrusel

---
+++ app/src/main/java/com/example/socios/ImagePagerAdapter.java
@@ -0,0 +1,47 @@
+package com.example.socios;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImagePagerAdapter extends PagerAdapter {
+
+    private Context context;
+    private int[] imageIds;
+
+    public ImagePagerAdapter(Context context, int[] imageIds) {
+        this.context = context;
+        this.imageIds = imageIds;
+    }
+
+    @Override
+    public int getCount() {
+        return imageIds.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_image, container, false);
+
+        ImageView imageView = view.findViewById(R.id.imageView);
+        imageView.setImageResource(imageIds[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
--- app/src/main/java/com/example/socios/MainActivity.java
+++ app/src/main/java/com/example/socios/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.socios;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
+import androidx.viewpager.widget.ViewPager;
+
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -11,6 +13,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private int[] imageIds = {R.drawable.group, R.drawable.group, R.drawable.group}; // Reemplaza con tus recursos de imágenes
+    private int currentPage = 0;
+    private static final long DELAY_MS = 3000; // 3 segundos
+    private Handler handler;
+
 
     EditText tieUsuario ;
     EditText tidPasswd;
@@ -22,10 +31,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewPager = findViewById(R.id.viewPager);
         tieUsuario = findViewById(R.id.tieUsuario);
         tidPasswd = findViewById(R.id.tidPasswd);
         imbtIngresar = findViewById(R.id.imbtIngresar);
         tvDatos = findViewById(R.id.tvDatos);
+
+        ImagePagerAdapter adapter = new ImagePagerAdapter(this, imageIds);
+        viewPager.setAdapter(adapter);
+
+        handler = new Handler();
+        startAutoPager();
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+
+            @Override
+            public void onPageSelected(int position) {
+                stopAutoPager();
+                currentPage = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {}
+        });
 
         imbtIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,9 +70,28 @@ public class MainActivity extends AppCompatActivity {
 
                     tvDatos.setText(tieUsuario.getText() + "\n" + tidPasswd.getText());
                 }
-
             }
         });
-
     }
+
+    private void startAutoPager() {
+        handler.postDelayed(autoPager, DELAY_MS);
+    }
+
+    private void stopAutoPager() {
+        handler.removeCallbacks(autoPager);
+    }
+
+    private final Runnable autoPager = new Runnable() {
+        public void run() {
+            if (currentPage == imageIds.length - 1) {
+                currentPage = 0;
+            } else {
+                currentPage++;
+            }
+            viewPager.setCurrentItem(currentPage, true);
+            startAutoPager();
+        }
+    };
+
 }
